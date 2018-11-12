168eb1ef6467f68552aae855163c791b5c5b3b88
Javier Alejandro Acevedo Barroso <jaalacba@hotmail.com>
2018-11-11 21:18:32 -0500

    to Cluster

--- plots.py
+++ plots.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-1c = np.loadtxt("1core.txt")
-2c = np.loadtxt("2core.txt")
-4c = np.loadtxt("4core.txt")
-8c = np.loadtxt("8core.txt")
+p1c = np.loadtxt("1core.txt")
+p2c = np.loadtxt("2core.txt")
+p4c = np.loadtxt("4core.txt")
+p8c = np.loadtxt("8core.txt")
 
 #plt.plot(times[:,1],times[:,0]/times[:,1])
-plt.plot(1c[:,1],1c[:,0]/1c[:,1],label = "1 core")
-plt.plot(2c[:,1],2c[:,0]/2c[:,1],label = "2 core")
-plt.plot(4c[:,1],4c[:,0]/4c[:,1],label = "4 core")
-plt.plot(8c[:,1],8c[:,0]/8c[:,1],label = "8 core")
+plt.plot(p1c[:,1],p1c[:,0]/p1c[:,1],label = "1 core")
+plt.plot(p2c[:,1],p2c[:,0]/p2c[:,1],label = "2 core")
+plt.plot(p4c[:,1],p4c[:,0]/p4c[:,1],label = "4 core")
+plt.plot(p8c[:,1],p8c[:,0]/p8c[:,1],label = "8 core")
 plt.legend()
 plt.title("Tiempo por archivo vs número de archivos")
 plt.xlabel("Número de archivos")
